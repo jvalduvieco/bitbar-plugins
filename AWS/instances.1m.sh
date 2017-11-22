dd2e90a3e6ddda79711a5132432cbede4be084ab
jvalduvieco <jvalduvieco@gmail.com>
2017-11-22 20:36:22 +0100

    Check instances in all regions. Handle 0 instances up.

--- AWS/instances.1m.sh
+++ AWS/instances.1m.sh
@@ -21,7 +21,7 @@ export PATH="/usr/local/bin:/usr/bin:/bin:$PATH"
 ## Required Configuration (must provide your own settings here)
 
 # AWS CLI credential profile
-AWS_CLI_PROFILE="eb-cli"
+AWS_CLI_PROFILE="default"
 
 ## Optional Configuration (not required)
 
@@ -37,13 +37,20 @@ OUT_SERVICE_COLOR="#ff0033"
 ## Implementation (changes optional, not required)
 
 # Fetch list of instances
-number_of_running_instances=$(aws --profile $AWS_CLI_PROFILE ec2 describe-instances --filters 'Name=instance-state-code,Values=16'| jq '.Reservations[].Instances | length')
-instances=$(aws --profile $AWS_CLI_PROFILE ec2 describe-instances --filters 'Name=instance-state-code,Values=16'|jq -r '.Reservations[].Instances[]| .PublicDnsName')
+for region in $(aws ec2 describe-regions --output text | cut -f3)
+do
+    number_of_running_instances=$(aws --profile ${AWS_CLI_PROFILE} ec2 describe-instances --region ${region} --filters 'Name=instance-state-code,Values=16'| jq '.Reservations[].Instances | length')
+ 	[[ -z ${number_of_running_instances} ]] && number_of_running_instances=0
+ 	instances=$(aws --profile $AWS_CLI_PROFILE ec2 describe-instances --region ${region} --filters 'Name=instance-state-code,Values=16'|jq -r '.Reservations[].Instances[]| .PublicDnsName')
+ 	total_number_of_running_instances=$((${total_number_of_running_instances} + ${number_of_running_instances}))
+ 	total_instances+="${instances[@]}"
+done
+
 
 # Output
-echo "$MENU_BAR_PREFIX_LABEL $number_of_running_instances"
+echo "$MENU_BAR_PREFIX_LABEL $total_number_of_running_instances"
 echo "---"
-for instance in $instances
+for instance in $total_instances
 do
 	echo "${instance} | color=$IN_SERVICE_COLOR"
 done
